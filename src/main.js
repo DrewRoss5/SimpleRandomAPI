06755a6257b2c3103cdeaffcfdf466a97eda0548
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-10-24 22:17:54 -0600

    fix formatting issues
    Added additional semicolons for consistency, replaced the use of "magic numbers" in argument indexing with the constants

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ var defaultArgs = [1, 100, 1, Date.now()*10000];
 
 // returns a random number given an API request and an RNG function
 function generateRandomNumber(req, func){
-    let args = [req.query.lower, req.query.upper, req.query.count, req.query.seed]
+    let args = [req.query.lower, req.query.upper, req.query.count, req.query.seed];
     // parse queries 
     // for each parameter (lower bound, upper bound, and seed), check if it's specified, and set it to the default if not
     for (let i = 0; i < 4; i++){
@@ -26,19 +26,19 @@ function generateRandomNumber(req, func){
     ran.core.seed(args[seedIndex]);
     // update the default seed
     defaultArgs[seedIndex] = Date.now()*10000;
-    console.log(`Random Number Generated between ${args[0]} and ${args[1]}, with seed ${args[3]}`)
+    console.log(`Generated ${args[countIndex]} random numbers between ${args[lBoundIndex]} and ${args[uBoundIndex]}, with seed ${args[seedIndex]}`);
     // convert output to an array if only one number is generated
     if (args[countIndex] < 2) {
-        return [func(args[lBoundIndex], args[uBoundIndex], args[countIndex])]
+        return [func(args[lBoundIndex], args[uBoundIndex], args[countIndex])];
     }
     else{
-        return func(args[lBoundIndex], args[uBoundIndex], args[countIndex])
+        return func(args[lBoundIndex], args[uBoundIndex], args[countIndex]);
     }
 }
 
-app.get('/int', (req, res) => {res.send(generateRandomNumber(req, ran.core.int))})
-app.get('/float', (req, res) => {res.send(generateRandomNumber(req, ran.core.float))})
+app.get('/int', (req, res) => {res.send(generateRandomNumber(req, ran.core.int))});
+app.get('/float', (req, res) => {res.send(generateRandomNumber(req, ran.core.float))});
 
 app.listen(port, () => {
-    console.log(`Random API listening on port ${port}`)
-})
+    console.log(`Random API listening on port ${port}`);
+});
